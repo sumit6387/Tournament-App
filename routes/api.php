ec2dc6609f1072bc20501e58ca03a71742dd83b8
heyakhil <54666562+heyakhil@users.noreply.github.com>
2020-11-06 06:38:48 -0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,17 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 
 Route::group(['prefix' => 'v1'],function(){
-        Route::post('/register',[LoginController::class , 'register']);
-        Route::post('/login',[LoginController::class , 'login']);
+    Route::post('/register',[LoginController::class , 'register']);
+    Route::post('/login',[LoginController::class , 'login']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function(){
         Route::post('/resendotp' , [LoginController::class , 'resendotp']);
         Route::post('/verifyotp',[LoginController::class , 'verifyOtp']);
-
-
-        Route::group(['middleware' => 'auth:sanctum'], function(){
-            
-            Route::get('/user',[UserController::class , 'user']);
-            Route::post('/uploadImage',[UserController::class , 'store']);
-            Route::post('/apply_ref_code',[UserController::class,'applyRefCode'])->middleware('CheckRefCode');  
-        });
+        Route::get('/user', [UserController::class , 'user']);
+        Route::post('/uploadImage',[UserController::class , 'store']);
+        Route::post('/apply_ref_code',[UserController::class,'applyRefCode'])
+        ->middleware('CheckRefCode');  
+    });
 });
 
 
