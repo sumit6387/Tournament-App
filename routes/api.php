a586fabf718e4b831b37eea08e6a104e22f67365
sumit6387 <54669901+sumit6387@users.noreply.github.com>
2020-12-03 07:08:38 +0530

    update #26
    update on define the what we get input in each route

--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,31 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ShowController;
 
 Route::group(['prefix' => 'v1','middleware' => 'CheckVersion'],function(){
-    Route::post('/register',[LoginController::class , 'register']);
-    Route::post('/login',[LoginController::class , 'login']);
-    Route::post('/verifyotp',[LoginController::class , 'verifyOtp']);
+    Route::post('/register',[LoginController::class , 'register']); //mobile_no,name, email,password,gender,ref_code
+    Route::post('/login',[LoginController::class , 'login']); //mobile_no , password
+    Route::post('/verifyotp',[LoginController::class , 'verifyOtp']); // mobile_no , otp
     
     Route::group(['middleware' => 'auth:sanctum','api'], function(){
-        Route::get('/user', [UserController::class , 'user']);
-        Route::post('/updatedata',[UserController::class , 'store']);
+        Route::post('/updatedata',[UserController::class , 'store']); //name , image,email,state,country,mobike_no,gender
         Route::post('/apply_ref_code',[UserController::class,'applyRefCode'])
-        ->middleware('CheckRefCode');  
-        Route::post('/joinTournament' , [UserController::class , 'joinTournament']);
-        Route::post('/addTournament' , [UserController::class , 'createUserTournament'])->middleware('CheckTournament');
-        Route::post('/updatePassword' , [UserController::class , 'updatePassword']);
+        ->middleware('CheckRefCode');  //ref_code
+        Route::post('/joinTournament' , [UserController::class , 'joinTournament']); //tournament_id
+        Route::post('/addTournament' , [UserController::class , 'createUserTournament'])->middleware('CheckTournament'); //prize_pool,winning,per_kill,entry_feeentry_fee,type,map,tournament_name,img,max_user_participated,game_type,tournament_type,tournament_name,tournament_start_date,tournament_start_time
+        Route::post('/updatePassword' , [UserController::class , 'updatePassword']); //tournament_id,user_id,password    in this route we update games id and password realtime
         Route::get('/claimPrize',[UserController::class , 'claimPrize']);
-        Route::post('/changePassword' , [UserController::class , 'changePassword']);
-        Route::post('/forgetPasswordProcess',[UserController::class , 'forgetPassword']);
-        Route::post('/forgetPassword' , [LoginController::class , 'resendotp']);
-        Route::post('/cancelMatch' , [UserController::class , 'cancelMatch']);  
+        Route::post('/changePassword' , [UserController::class , 'changePassword']); // current_password,new_password , confirm_password
+        Route::post('/forgetPasswordProcess',[UserController::class , 'forgetPassword']); //mobile_no,otp,password
+        Route::post('/forgetPassword' , [LoginController::class , 'resendotp']); //mobile_no
+        Route::post('/cancelMatch' , [UserController::class , 'cancelMatch']);   //tournament_id
         
         // payment route for join tournament
-        Route::post('/payment-request' , [PaymentController::class , 'createPaymentOrder']);
-        Route::post('/payment-complete', [PaymentController::class , 'paymentComplete']);
+        Route::post('/payment-request' , [PaymentController::class , 'createPaymentOrder']); //amount
+        Route::post('/payment-complete', [PaymentController::class , 'paymentComplete']); //razorpay_payment_id,razorpay_order_id,razorpay_signature
 
 
         // payment route for membership
         Route::post('/payment-request-membership' , [PaymentController::class , 'createMembershipOrder']);
-        Route::post('/payment-complete-membership', [PaymentController::class , 'paymentCompleteMembership']);
+        Route::post('/payment-complete-membership', [PaymentController::class , 'paymentCompleteMembership']);//razorpay_payment_id,razorpay_order_id,razorpay_signature
 
         // show data route
         Route::get('/showtournament' , [ShowController::class , 'showTournaments']);
@@ -43,7 +42,7 @@ Route::group(['prefix' => 'v1','middleware' => 'CheckVersion'],function(){
         Route::get('/allPoint' , [ShowController::class , 'allPoint']);
         Route::get('/referAndEarn' , [ShowController::class , 'referAndEarn']);
         Route::get('/ourTournament' , [ShowController::class , 'ourTournament']);
-        Route::post('/tournamentDetail' , [ShowController::class , 'tournamentDetail']);
+        Route::post('/tournamentDetail' , [ShowController::class , 'tournamentDetail']); //tournament_id
     });
     Route::fallback(function(){
         return response()->json([
